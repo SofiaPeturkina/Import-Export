b3e65266f624427e916c622b36c4a3cf86bbf472
Sofya Peturkina <85740669+SofiaPeturkina@users.noreply.github.com>
2022-02-15 19:03:08 +0300

    Solution Task Import-Export

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+import defaultGame, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame} from './game';
+
+console.log("app worked");
+
+const game = new Game();
+game.start();
